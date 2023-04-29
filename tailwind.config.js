9d6e7d4d24408e85b77c3702652127553b1d35f0
antoine <antoine.caputo1@viacesi.fr>
2023-04-29 02:19:25 +0200

    [UPDATE] Integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ["./**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#00B382',
+        'secondary': '#21BC81',
+        'alternative' : '#000D46',
+        'detail': '#F1A34F',
+      }
+    },
+    fontFamily: {
+    }
   },
   plugins: [],
 
