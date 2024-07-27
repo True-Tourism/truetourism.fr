be4028d725bde6a2629987b238d029775c0db1ef
Antoine Caputo <antoine.caputo@lenstra.fr>
2024-07-27 20:12:28 +0200

    feat: add landing images

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'primary': '#00B382',
         'secondary': '#279884',
         'alternative' : '#000D46',
-        'detail': '#F1A34F',
+        'detail': '#ff7700',
       }
     },
     fontFamily: {
