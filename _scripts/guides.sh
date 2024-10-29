d1196c13a29415237093a7686af67684eabec372
Antoine Caputo <antoine.caputo@lenstra.fr>
2024-10-29 22:57:23 +0100

    feat(workflow): update guides

--- _scripts/guides.sh
+++ _scripts/guides.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-: '
-# Exclude test accounts
-query GetGuides {
+set -e
+# Ensure jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found, please install jq to run this script."
+    exit
+fi
+
+json_file="guides.json"
+
+# Exclude test accounts and accounts with no visits
+# shellcheck disable=SC2089
+query='query GetGuides {
   accounts_guide(where: {
     documents_verified: {_eq: true},
     user_id: {_nin: ["cb1b0547-22b3-4fc5-aa80-b705badc1770", "bf1db3e5-b8f3-471a-9efa-3a55a8e0ea1b"]},
@@ -30,15 +40,29 @@ query GetGuides {
 }
 '
 
-# Ensure jq is installed
-if ! command -v jq &> /dev/null
-then
-    echo "jq could not be found, please install jq to run this script."
+# Write query into a json file
+stringify_query=$(jq -n --arg query "$query" '$query')
+echo "{\"query\": $stringify_query}" > query.json
+
+# Get the data from the GraphQL endpoint
+curl --request POST \
+  --url https://"$NHOST_GRAPHQL_URL"/v1 \
+  --header 'Content-Type: application/json' \
+  --header "X-Hasura-Admin-Secret: $HASURA_GRAPHQL_ADMIN_SECRET" \
+  --header 'x-hasura-role: admin' \
+  --output "$json_file" \
+  --data @query.json
+
+echo "Response: $(cat guides.json)"
+
+# Detect error from the response
+if [ ! -s "$json_file" ]; then
+    echo "Error in the response from the GraphQL endpoint."
+    if jq -e .errors "$json_file" > /dev/null; then
+        echo "$(jq -r '.errors' "$json_file")"
+    fi
     exit
 fi
-
-# Read the JSON file
-json_file="guides.json"
 
 # Extract the data from the JSON file using jq
 jq -c '.data.accounts_guide[]' "$json_file" | while read -r user; do
@@ -46,6 +70,8 @@ jq -c '.data.accounts_guide[]' "$json_file" | while read -r user; do
     avatarUrl=$(echo "$user" | jq -r '.user.avatarUrl')
     displayName=$(echo "$user" | jq -r '.user.displayName')
     bio=$(echo "$user" | jq -r '.user.metadata')
+
+    echo "Processing user: $displayName - $user_id"
 
     # Initialize visits array
     visits=$(echo "$user" | jq -c '.user.visits[]' | while read -r visit; do
@@ -74,7 +100,7 @@ EOV
     output_file="${user_id}.md"
 
     # Write the content to the file
-    cat <<EOF > "$output_file"
+    cat <<EOF > "_guides/$output_file"
 ---
 layout: guide
 avatarUrl: $avatarUrl
@@ -85,5 +111,3 @@ $visits
 ---
 EOF
 done
-
-echo "Files created successfully."
